895a8e65c7a45c8105c3f4bc7cc0cfb171cb18ba
Tracy Chiappari <tracy.chiappari@gmail.com>
2025-11-18 02:49:43 -0800

    GH-11: Enable cors so frontend can call backen api

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var db = require('./db/mysql');
@@ -14,6 +15,7 @@ async function initializeApp() {
         // Connect to MongoDB
         await db.connect();
 
+        app.use(cors());
         app.use(logger('dev'));
         app.use(express.json());
         app.use(express.urlencoded({ extended: false }));
