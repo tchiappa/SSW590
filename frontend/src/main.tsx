d1be37120fbbb222ed994569c129529cc618c81f
Tracy Chiappari <tracy.chiappari@gmail.com>
2025-11-18 02:52:48 -0800

    GH-11: Add degree and certificate select dropdowns to main page.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import {Heading} from "./components/heading.tsx";
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import App from './App'
+import {Heading} from "./ui/heading";
+import {DegreeSelect} from "./components/DegreeSelect";
+import {CertificateSelect} from "./components/CertificateSelect.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <App>
           <Heading>SSW590 - Group Project</Heading>
+          <DegreeSelect/>
+          <CertificateSelect/>
       </App>
   </StrictMode>,
 )
