1f7b90cde2322559bee6d27559227fcd42395503
Tracy Chiappari <tracy.chiappari@gmail.com>
2025-11-19 01:16:05 -0800

    GH-11: Refactored the App components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
-import {Navbar, NavbarItem, NavbarSection} from './ui/navbar'
+import { Navbar, NavbarItem, NavbarSection } from './ui/navbar'
 import { Sidebar } from './ui/sidebar'
 import { StackedLayout } from './ui/stacked-layout'
+import { Heading } from "./ui/heading.tsx";
+import { DegreeSelect } from "./components/DegreeSelect.tsx";
+import { CertificateSelect } from "./components/CertificateSelect.tsx";
+import { ClassList } from "./components/ClassList.tsx";
+import { useState } from "react";
 
-export default function App({ children }:{ children: any; }) {
+export default function App() {
+    const [selectedDegree, setSelectedDegree] = useState<string>('');
+    const [selectedCertificate, setSelectedCertificate] = useState<string>('');
+
     return (
         <StackedLayout
             navbar={<Navbar><NavbarSection>
@@ -10,7 +18,10 @@ export default function App({ children }:{ children: any; }) {
             </NavbarSection></Navbar>}
             sidebar={<Sidebar></Sidebar>}
         >
-            {children}
+            <Heading>SSW590 - Group Project</Heading>
+            <DegreeSelect value={selectedDegree} onChange={setSelectedDegree}/>
+            <CertificateSelect value={selectedCertificate} onChange={setSelectedCertificate}/>
+            <ClassList degreeId={selectedDegree} certificateId={selectedCertificate}/>
         </StackedLayout>
     )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,9 @@
-import {StrictMode, useState} from 'react'
-import {createRoot} from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App'
-import {Heading} from "./ui/heading";
-import {DegreeSelect} from "./components/DegreeSelect";
-import {CertificateSelect} from "./components/CertificateSelect.tsx";
-import {ClassList} from "./components/ClassList.tsx";
-
-function Main() {
-    const [selectedDegree, setSelectedDegree] = useState<string>('');
-    const [selectedCertificate, setSelectedCertificate] = useState<string>('');
-
-    return (
-        <App>
-            <Heading>SSW590 - Group Project</Heading>
-            <DegreeSelect value={selectedDegree} onChange={setSelectedDegree}/>
-            <CertificateSelect value={selectedCertificate} onChange={setSelectedCertificate}/>
-            <ClassList degreeId={selectedDegree} certificateId={selectedCertificate}/>
-        </App>
-    );
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Main/>
+      <App/>
   </StrictMode>,
 )
